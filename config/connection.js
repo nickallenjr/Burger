a4a7aa334c224583064beffe5b56ca57bcd7a20f
Nicholas <nicholasallenjr@gmail.com>
2017-10-03 13:43:59 -0400

    updated local server login

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 var mysql = require('mysql');
-var connection;
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
+		port: 3306,
 		host: 'localhost',
-		password: 'hacktheplanet',
+		password: 'root',
 		user: 'root',
-		database: 'todoagain_db'
+		database: 'burgers_db'
 	});
 };
 
